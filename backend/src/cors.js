de5d9843eb916fa52a02133c311a9a1426d780af
Arish03 <arish14022002@gmail.com>
2026-03-26 16:52:55 +0530

    first commit

---
+++ backend/src/cors.js
@@ -0,0 +1,13 @@
+import FastifyCors from "@fastify/cors";
+
+export const setFastifyCors = function (fastify) {
+  fastify.register(FastifyCors, {
+    // put your options here
+    origin: [
+      "http://localhost:9000",
+      "http://localhost:8100",
+      "http://localhost:4200",,
+      "http://localhost:4202",
+    ],
+  });
+};
